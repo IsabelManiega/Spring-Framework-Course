0e9bedfcf2e26759aee92f7b96df3340b275b6fa
IsabelManiega <isabelmaniega@hotmail.com>
2018-09-01 14:53:17 +0200

    First Project: Eclipse-Spring

---
+++ src/main/java/com/bolsadeideas/springboot/app/SpringBootWebHolaMundoApplication.java
@@ -0,0 +1,12 @@
+package com.bolsadeideas.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebHolaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebHolaMundoApplication.class, args);
+	}
+}
