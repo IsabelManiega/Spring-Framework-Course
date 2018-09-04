ab7e730fd4411b460b94b6b5a7d1220d44af8690
IsabelManiega <isabelmaniega@hotmail.com>
2018-09-04 14:11:07 +0200

    Spring-Thymeleaf

---
+++ spring-boot-web-thymeleaf-1/src/main/java/com/bolsadeideas/springboot/app/SpringBootWebThymeleaf1Application.java
@@ -0,0 +1,12 @@
+package com.bolsadeideas.springboot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebThymeleaf1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebThymeleaf1Application.class, args);
+	}
+}
